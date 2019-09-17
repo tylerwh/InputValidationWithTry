e578babd79e0d32d80b2d35c8b8fa08382c77540
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-09-16 21:09:53 -0500

    Added third test to test for negative number found in the third argument of average()

--- test/test_validation_with_try.py
+++ test/test_validation_with_try.py
@@ -14,6 +14,11 @@ class MyTestCase(unittest.TestCase):
   def test_average_negative_input_index_one(self):
     with self.assertRaises(ValueError):
       validation_with_try.average(90, -90, 99)
+  
+
+  def test_average_negative_input_index_two(self):
+    with self.assertRaises(ValueError):
+      validation_with_try.average(90, 90, -99)
 
 
 if __name__ == "__main__":
