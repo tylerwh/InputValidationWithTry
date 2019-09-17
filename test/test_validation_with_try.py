d007b04d35f75efc18c628d0bd517ab6687b943a
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-09-16 20:46:09 -0500

    Further Clean-up to start file. Test written and not passing.

--- test/test_validation_with_try.py
+++ test/test_validation_with_try.py
@@ -1,6 +1,6 @@
 import unittest
 import unittest.mock as mock
-from input_validation import validation_with_try as validation_with_try
+from input_validation import validation_with_try
 
 
 class MyTestCase(unittest.TestCase):
